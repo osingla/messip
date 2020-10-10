ed62298aa6e9281471667456668b64ad86a4e0ef
Olivier Singla <osingla@localhost.localdomain>
2020-10-10 19:07:15 -0400

    work in progress

--- examples/Src/messip_example_8.c
+++ examples/Src/messip_example_8.c
@@ -24,6 +24,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <getopt.h>
+#include <sys/wait.h>
 
 #include "messip.h"
 
@@ -47,11 +48,12 @@ static messip_channel_t *create_channel( messip_cnx_t *cnx, char *name )
 
 static int server( int argc, char *argv[] ) {
 	/*--- Connect to one messip server ---*/
-	display( "Start process\n" );
+	display( "Server", "Start process\n" );
 	messip_init( );
     messip_cnx_t *cnx = messip_connect( NULL, "ex8/p1", MESSIP_NOTIMEOUT );
-    if ( !cnx ) 
+    if ( !cnx ) {
         cancel( "Unable to find messip server\n" );
+    }
 
 	/*--- Create 5 channels ---*/
 	messip_channel_t *ch1 = create_channel( cnx, "one" );
@@ -61,10 +63,10 @@ static int server( int argc, char *argv[] ) {
 	messip_channel_t *ch5 = create_channel( cnx, "five" );
 
 	/*--- Now delete the 3rd channel ---*/
-	display( "Channels have been created. Waiting 15 seconds...\n" );
+	display( "Server", "Channels have been created. Waiting 15 seconds...\n" );
 	delay( 15000 );
 	int status = messip_channel_delete( ch3, MESSIP_NOTIMEOUT );
-	display( "messip_channel_delete #3: status=%d\n", status );
+	display( "Server", "messip_channel_delete #3: status=%d\n", status );
 	assert(status == 0);
 	delay( 15000 );
 
@@ -72,18 +74,18 @@ static int server( int argc, char *argv[] ) {
 	messip_channel_t *xch4 = messip_channel_connect( cnx, "four", MESSIP_NOTIMEOUT );
 	assert(xch4 != NULL);
 	status = messip_channel_delete( ch4, MESSIP_NOTIMEOUT );
-	display( "messip_channel_delete #4: status=%d\n", status );
+	display( "Server", "messip_channel_delete #4: status=%d\n", status );
 	assert(status == 1);
 
 	/*--- Now delete the 4th channel ---*/
 	status = messip_channel_disconnect( xch4, MESSIP_NOTIMEOUT );
-	display( "messip_channel_disconnect #4: status=%d\n", status );
+	display( "Server", "messip_channel_disconnect #4: status=%d\n", status );
 	status = messip_channel_delete( ch4, MESSIP_NOTIMEOUT );
-	display( "messip_channel_delete #4: status=%d\n", status );
+	display( "Server", "messip_channel_delete #4: status=%d\n", status );
 	assert(status == 0);
 
 	/*--- Now wait for one message on this on channel ---*/
-	display( "Wait now 10 seconds...\n" );
+	display( "Server", "Wait now 10 seconds...\n" );
 	delay( 10000 );
 	char rec_buff[ 80 ];
 	memset( rec_buff, 0, sizeof(rec_buff) );
@@ -94,11 +96,11 @@ static int server( int argc, char *argv[] ) {
 		fprintf( stderr, "Error on receive message on channel '%s'\n", "one" );
 		return -1;
 	}
-	display( "received %d-%d:'%s' type=%d from id=%s index=%d\n", 
+	display( "Server", "received %d-%d:'%s' type=%d from id=%s index=%d\n", 
 		ch1->datalen, ch1->datalenr, rec_buff, type, ch1->remote_id, index );
 	assert( !strcmp( rec_buff, "Hello" ) );
 	assert( type == 1961 );
-	display( "...waiting 10 seconds before replying to the client...\n" );
+	display( "Server", "...waiting 10 seconds before replying to the client...\n" );
 	delay( 10000 );
 	messip_reply( ch1, index, 3005, "Bonjour", 8, MESSIP_NOTIMEOUT );
 
@@ -110,11 +112,11 @@ static int server( int argc, char *argv[] ) {
 		fprintf( stderr, "Error on receive message on channel '%s'\n", "one" );
 		return -1;
 	}
-	display( "received %d-%d:'%s' type=%d from %s index=%d\n", 
+	display( "Server", "received %d-%d:'%s' type=%d from %s index=%d\n", 
 		ch1->datalen, ch1->datalenr, rec_buff, type, ch1->remote_id, index );
 	assert(index==MESSIP_MSG_DISCONNECT || index==MESSIP_MSG_DISMISSED);
 
-	display( "End process\n" );
+	display( "Server", "End process\n" );
 	return 0;
 }                               // server
 
@@ -128,20 +130,22 @@ static int server( int argc, char *argv[] ) {
 static int client( int argc, char *argv[] ) {
 	/*--- Connect to one messip server ---*/
 	messip_init( );
-	display( "start process\n" );
+	display( "Client", "start process\n" );
     messip_cnx_t *cnx = messip_connect( NULL, "ex8/p2", MESSIP_NOTIMEOUT );
-    if ( !cnx ) 
+    if ( !cnx ) {
         cancel( "Unable to find messip server\n" );
+    }
 
 	/*--- Localize channel 'one' ---*/
 	messip_channel_t *ch = messip_channel_connect( cnx, "one", MESSIP_NOTIMEOUT );
-	if ( !ch ) 
+	if ( !ch ) {
 		cancel( "Unable to localize channel '%s'\n", "one" );
-	display( "Channel located - remote_id=%s\n", 
+    }
+	display( "Client", "Channel located - remote_id=%s\n", 
 		ch->remote_id );
 
 	/*--- Send a message on channel 'one' ---*/
-	display( "sending now a message to the server...\n" );
+	display( "Client", "sending now a message to the server...\n" );
 	char rec_buff[ 80 ];
 	memset( rec_buff, 0, sizeof( rec_buff ) );
     int32_t answer;
@@ -149,17 +153,17 @@ static int client( int argc, char *argv[] ) {
 		1961, "Hello", 6, 
 		&answer, rec_buff, sizeof( rec_buff ), 
 		MESSIP_NOTIMEOUT );
-	display( "send status=%d received back=%d-%d:'%s' answer=%d  id=%s\n", 
+	display( "Client", "send status=%d received back=%d-%d:'%s' answer=%d  id=%s\n", 
 		status, ch->datalen, ch->datalenr, rec_buff, answer, ch->remote_id );	
 	assert( answer == 3005 );
 
 	/*--- Then 2 ping messages ---*/
 	delay( 3000 );
-	display( "Waiting for ping...\n" );
+	display( "Client", "Waiting for ping...\n" );
 	messip_channel_ping( ch, 5000 );
 	delay( 3000 );
 	messip_channel_ping( ch, 5000 );
-	display( "Waiting for ping...\n" );
+	display( "Client", "Waiting for ping...\n" );
 
 	/*--- Disconnect from this channel ---*/
 	messip_channel_disconnect( ch, MESSIP_NOTIMEOUT );
